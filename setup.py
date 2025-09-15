1c15c8f29c44ba21e41b18f491225edb9f63558f
Rasa Dariush <letperhut@gmail.com>
2025-09-15 15:33:03 +0330

    Initial updates

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pebble-lang",
     version="1.0.0",
     description="Pebble programming language interpreter in Python",
+    long_description=long_description,         # <-- add this
+    long_description_content_type="text/markdown",  # <-- important
     author="Rasa8877",
     author_email="letperhut@gmail.com",
-    url="https://github.com/Rasa8877/Pebble-lang",
+    url="https://github.com/Rasa8877/pebble-lang",
     packages=find_packages(),
     python_requires=">=3.8",
     entry_points={
